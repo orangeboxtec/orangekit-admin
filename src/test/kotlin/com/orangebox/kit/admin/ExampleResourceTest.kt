2e22a6ab337ea000948334403d0f13c5f5672610
Diego Belli Mondego <diego@orangebox.technology>
2023-02-22 16:14:15 -0300

    fix file structure

--- src/test/kotlin/com/orangebox/kit/admin/ExampleResourceTest.kt
+++ src/test/kotlin/com/orangebox/kit/admin/ExampleResourceTest.kt
@@ -11,10 +11,10 @@ class ExampleResourceTest {
     @Test
     fun testHelloEndpoint() {
         given()
-            .`when`().get("/hello")
-            .then()
-            .statusCode(200)
-            .body(`is`("Hello from RESTEasy Reactive"))
+          .`when`().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(`is`("Hello from RESTEasy Reactive"))
     }
 
 }
