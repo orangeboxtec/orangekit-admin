baf1ede46a0782be3d7f0053b4f697ed46ffec28
Diego Mondego <diego@orangebox.technology>
2023-05-18 13:34:36 -0300

    login by username

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.orangebox.kit.admin"
-version = "1.0.30"
+version = "1.0.31"
 
 repositories {
     mavenCentral()
@@ -28,6 +28,7 @@ dependencies {
     implementation("io.quarkus:quarkus-arc")
     implementation("io.quarkus:quarkus-resteasy-reactive")
     implementation("com.jayway.jsonpath:json-path:2.8.0")
+    implementation("org.mongodb:bson:4.9.1")
 
     implementation("com.orangebox.kit.core:orangekit-core:1.0.14")
     implementation("com.orangebox.kit.authkey:orangekit-authkey:1.0.1")
